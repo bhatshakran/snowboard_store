909b0645f2d5ae96df43bc0adb58ab310c8984c0
bhatshakran <bhatshakran@gmail.com>
2022-11-13 23:07:40 +0530

    Created product features component

--- components/mountainCard.tsx
+++ components/mountainCard.tsx
@@ -1,10 +1,13 @@
 import gsap from 'gsap';
 import React from 'react';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import StarIcon from '@mui/icons-material/Star';
 
 const MountainCard = () => {
-  const overlayRef = React.useRef<HTMLDivElement>(null);
+  // const overlayRef = React.useRef<HTMLDivElement>(null);
 
-  React.useEffect(() => {
+  /*  React.useEffect(() => {
     gsap.set(overlayRef.current, { y: '300', x: '300', opacity: 0 });
     gsap.to(overlayRef.current, {
       y: 0,
@@ -18,29 +21,32 @@ const MountainCard = () => {
         toggleActions: 'restart none none none',
       },
     });
-  }, []);
-  return (
-    <div className='mountain_card bg-red-100 mt-80 mb-44 relative overflow-hidden'>
-      <div
-        ref={overlayRef}
-        className='overlay_txt p-6 overflow-hidden lg:rounded-lg sm:pt-12'
-      >
-        <h2 className='text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-poppins font-bold'>
-          THE MOUNTAINS SNOWBOARDING
-        </h2>
-        <p className='text-sm  sm:text-lg mt-8 xl:mt-12 font-poppins'>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
-          laudantium eius ex, iusto ratione adipisci tempore nemo qui ullam
-          beatae commodi necessitatibus incidunt tenetur perferendis voluptate
-          omnis ipsa neque! A enim voluptate, laborum, deleniti praesentium vel
-          exercitationem facilis aliquid nemo sit cum recusandae ad culpa? Illo
-          repellat numquam porro molestiae harum accusamus suscipit tempore
-          atque deleniti, libero delectus eos maiores, assumenda debitis odit
-          laudantium praesentium dolorum recusandae cum, excepturi tempora unde
-          vel! Assumenda ea sed dolorem, dolore fugiat ratione. Nesciunt laborum
-          doloremque vero omnis velit non saepe tempore quia aperiam porro nobis
-          itaque fugit incidunt, veritatis, voluptatem atque. Ea, possimus.
-        </p>
+  }, []) */ return (
+    <div className='  mt-40 mb-44 flex justify-center overflow-hidden w-full'>
+      <div className='flex max-w-4xl gap-4 justify-center text-left'>
+        <div className='flex flex-col items-center  gap-4 w-1/3 border-r px-4'>
+          <LocalShippingIcon className=' text-green-500' />
+          <h3 className='text-xl font-poppins font-bold'>Free shipping</h3>
+          <p className='font-poppins font-light'>
+            Order anything above 30$ and get free shipping on our products.
+          </p>
+        </div>
+        <div className='flex flex-col items-center  gap-4 w-1/3 border-r px-4'>
+          <KeyboardReturnIcon className=' text-green-500' />
+          <h3 className='text-xl font-poppins font-bold'>30 Day Return </h3>
+          <p className='font-poppins font-light'>
+            When returning the items, we do a refund of the order.You can also
+            exchange the order with different item.
+          </p>
+        </div>
+        <div className='flex flex-col items-center gap-4 w-1/3  px-4'>
+          <StarIcon className=' text-green-500 ' />
+          <h3 className='text-xl font-poppins font-bold'>Best Quality</h3>
+          <p className='font-poppins font-light'>
+            We only sell items from renowed and top quality brands.We also
+            ensure quality packing and shipping.
+          </p>
+        </div>
       </div>
     </div>
   );
